f93cb5e79480afa5ae973f99380e889168fefce0
danilo neves cruz <cruzdanilo@gmail.com>
2025-11-07 11:25:32 -0300

    💬 add html metadata

--- src/sequence.ts
+++ src/sequence.ts
@@ -2,9 +2,9 @@ import { createConfig } from "@0xsequence/connect";
 import { allNetworks, ChainId } from "@0xsequence/network";
 
 const { connectConfig, wagmiConfig } = createConfig("waas", {
-  appName: "Exa App Embed Example",
+  appName: "Exa App embed example",
   defaultChainId: ChainId.OPTIMISM,
-  embeddedWalletTitle: "Exa App Owner Wallet",
+  embeddedWalletTitle: "Exa App owner wallet",
   projectAccessKey: "AQAAAAAAAKvGuTfXGGCZM2jb2kG_7W3qVGk", // cspell:ignore AQAAAAAAAKvGuTfXGGCZM2jb2kG_7W3qVGk
   googleClientId: "27593622387-9dnoiu04rvha804eeucloh6hkaqjk6hh.apps.googleusercontent.com", // cspell:ignore 9dnoiu04rvha804eeucloh6hkaqjk6hh.apps.googleusercontent.com
   waasConfigKey: "eyJwcm9qZWN0SWQiOjQzOTc0LCJycGNTZXJ2ZXIiOiJodHRwczovL3dhYXMuc2VxdWVuY2UuYXBwIn0=",
